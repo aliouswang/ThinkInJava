b1fcf4809187be2857c3e0bc7cdbac00d65974ea
aliouswang <wsw622@126.com>
2016-09-18 23:30:26 +0800

    init commit

---
+++ simple/src/main/java/com/alious/thinkinjava/ch14/Candy.java
@@ -0,0 +1,7 @@
+package com.alious.thinkinjava.ch14;
+
+/**
+ * Created by aliouswang on 16/9/18.
+ */
+public class Candy {
+}
--- simple/src/main/java/com/alious/thinkinjava/ch14/Main.java
+++ simple/src/main/java/com/alious/thinkinjava/ch14/Main.java
@@ -1,14 +1,12 @@
 package com.alious.thinkinjava.ch14;
 
-import java.util.ArrayList;
-
 /**
  * Created by aliouswang on 16/9/13.
  */
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList();
+
     }
 
 }
